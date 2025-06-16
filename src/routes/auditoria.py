c07e395e2f699d936b9774366b2780bd828445d4
Ferraz <warley.ferraz.wf@gmail.com>
2025-06-16 01:29:29 -0300

    implementando auditoria 2

--- src/routes/auditoria.py
+++ src/routes/auditoria.py
@@ -1,9 +1,12 @@
+#/src/routes/auditoria.py
 from flask import Blueprint, render_template
 from src.models.auditoria import Auditoria
+from flask_login import login_required
 
 auditoria_bp = Blueprint('auditoria', __name__)
 
 @auditoria_bp.route('/admin/auditoria')
+@login_required
 def painel_auditoria():
     logs = Auditoria.query.order_by(Auditoria.data_hora.desc()).limit(100).all()
     return render_template('admin/auditoria.html', logs=logs)
